3b6789bc1ef665661945f673e85153c13c4755ef
JS <csdnum0@gmail.com>
2024-09-06 11:14:21 +0900

    d

--- src/main/java/com/app/clusterapp/controller/HelloController.java
+++ src/main/java/com/app/clusterapp/controller/HelloController.java
@@ -1,20 +1,24 @@
 package com.app.clusterapp.controller;
 
 import java.time.LocalDateTime;
+import org.springframework.ui.Model;
 
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-@RestController
+//@RestController // if it is for only backend service
+@Controller // if it is for showing thymleaf ui 
 public class HelloController {
     @GetMapping("/")
-    public String hello() {
+    public String hello(Model model) {
         // 현재 시간 가져오기
         LocalDateTime currentTime = LocalDateTime.now();
         
         // 기본 형식으로 출력
-        System.out.println("현재 시간: " + currentTime);
+        System.out.println("2.0 현재 시간: " + currentTime);
 
-        return "Hello, Kubernetes! " + currentTime;
+//        return "2.0 Hello, Kubernetes! " + currentTime;
+        return "index";
     }
 }
